ae1774964407bd0743888d4d1bbab1e6d56f671d
Gabriel26-univille <132339190+Gabriel26-univille@users.noreply.github.com>
2023-06-29 20:13:03 -0300

    Atualização
    Adicionada função que limpa a pasta resources.
    
    Co-authored-by: brunorichartzf <bruno.farias@univille.br>

--- src/BlocoDeNotas.java
+++ src/BlocoDeNotas.java
@@ -1,8 +1,6 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.io.*;
-import java.util.*;
 
 public class BlocoDeNotas{
     static HashMap<String,Nota> BlocoDeNotas = new HashMap<>();
@@ -13,84 +11,23 @@ public class BlocoDeNotas{
         BlocoDeNotas.put(titulo,nota);
     }
 
-    public void Apagar(int id){
-        BlocoDeNotas.get(id).Apagar(BlocoDeNotas.get(id).getPath());
-        BlocoDeNotas.remove(id);
+    public void Apagar(String titulo){
+        BlocoDeNotas.get(titulo).Apagar(BlocoDeNotas.get(titulo).getPath());
+        BlocoDeNotas.remove(titulo);
     }
 
     public String getNota(String titulo){
         return BlocoDeNotas.get(titulo).getTitulo() + BlocoDeNotas.get(titulo).getTexto();
     }
 
-    public static void salvar(){
-        File file = new File(output);
-        BufferedWriter bf = null;
+    public static void ApagaTudo(){
 
-        try{
-            bf = new BufferedWriter(new FileWriter(file));
-
-            for (Map.Entry<String, Nota> entry :
-                    BlocoDeNotas.entrySet()) {
-
-                bf.write(entry.getKey() + ":"
-                        + entry.getValue());
-
-                bf.newLine();
-            }
-            bf.flush();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        finally {
-
-            try {
-                bf.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
-    /*
-    public carregar(){
-        File file = new File(output);
-        BufferedReader br = null;
-
-        try {
-            br = new BufferedReader(new FileReader(file));
-            String line = null;
-
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(":");
-
-                String titulo = parts[0].trim();
-                String texto = parts[1].trim();
-                if (!titulo.equals("") && !texto.equals(""))
-                    BlocoDeNotas.put(titulo, texto);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        finally {
-
-            // Always close the BufferedReader
-            if (br != null) {
-                try {
-                    br.close();
-                }
-                catch (Exception e) {
-                };
-            }
-        }
-
-        return BlocoDeNotas;
-
-        }
-*/
-
-    public static void mostrar(){
     for (String name : BlocoDeNotas.keySet()) {
-        System.out.println(name);
+        try{
+            Files.delete(Paths.get("resources/"+name+".txt"));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         BlocoDeNotas Bloco = new BlocoDeNotas();
+        System.out.print("Digite o título da primeira nota: ");
         String input = leitor.nextLine();
-
         String tit = input;
+        System.out.print("Digite o conteúdo da primeira nota: ");
         input = leitor.nextLine();
         String tex = input;
         Bloco.Anotar(tit, tex);
-        System.out.println(Bloco.getNota(tit));
-        //int inputao = leitor.nextInt();
-        //int id = inputao;
-        //Bloco.Apagar(inputao);
-        //System.out.println(Bloco.BlocoDeNotas.isEmpty());
-        BlocoDeNotas.salvar();
-        BlocoDeNotas.mostrar();
+
+        System.out.print("Digite o título da segunda nota: ");
+        input = leitor.nextLine();
+        tit = input;
+        System.out.print("Digite o conteúdo da segunda nota: ");
+        input = leitor.nextLine();
+        tex = input;
+        Bloco.Anotar(tit, tex);
+
+        System.out.print("Digite enter para finalizar:");
+        leitor.nextLine();
+
+        BlocoDeNotas.ApagaTudo();
     }
 }
